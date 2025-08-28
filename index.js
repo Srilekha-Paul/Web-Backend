b7c946f3fae4cb5f24d93547fe562fb7b82dfcb4
Srilekha Paul <srilekhapaul2003@gmail.com>
2025-08-29 02:38:14 +0530

    config dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,7 +19,7 @@ app.get('/youtube', (req, res) => {
 app.get('/login', (req, res) => {
     res.send('<h1>please login at coding<h1>')
 })
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
